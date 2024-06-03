a2eebfd9be697bd6520e65b440c16da2583fc689
luwam <lwamyohannes2018@gmail.com>
2024-06-03 01:16:22 -0600

    css modification

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
-import { getImageUrl } from '../../utils'
-import styles from "./Contact.module.css";
+import React from 'react';
+import { getImageUrl } from '../../utils';
+import styles from './Contact.module.css';
+
 export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
         <h2>Contact</h2>
-        <p>feel free to reach out</p>
+        <form action="https://formspree.io/f/xoqgownw" method="POST">
+          <input type="text" name="subject" placeholder="Your Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name="message" placeholder="Message" required></textarea>
+          <input className={styles.button} id="submit" type="submit" value="Send" />
+        </form>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")}  alt='Email Icon'/>
-          <a href='mailto:lwamyohannes2018@gmail.com'>myemail@gmail.com</a>
+          <a href='mailto:lwamyohannes2018@gmail.com'>
+            <img src={getImageUrl("contact/emailIcon.png")} alt='Email Icon' />
+          </a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/linkedinIcon.png")}  alt='Email Icon'/>
-          <a href='https://www.linkedin.com/in/luwam-y-beyin-86b03b214/'>linkin.com</a>
+          <a href='https://www.linkedin.com/in/luwam-y-beyin-86b03b214/'>
+            <img src={getImageUrl("contact/linkedinIcon.png")} alt='LinkedIn Icon' />
+          </a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/githubIcon.png")}  alt='Email Icon'/>
-          <a href='https://github.com/merinnaa'>github.com</a>
+          <a href='https://github.com/merinnaa'>
+            <img src={getImageUrl("contact/githubIcon.png")} alt='GitHub Icon' />
+          </a>
+          <li className={styles.link}>
+          <a href='https://x.com/YohannesLw69405'>
+            <img src={getImageUrl("contact/twitterIcon.png")} alt='Twitter Icon' />
+          </a>
+        </li>
         </li>
       </ul>
+      <div className={styles.copyright}>
+        <p>&copy; 2024 copyright all rights reserved</p>
+      </div>
     </footer>
-  )
-}
+  );
+};
--- src/component/Experience/Experience.jsx
+++ src/component/Experience/Experience.jsx
@@ -10,7 +10,7 @@ export const Experience = () => {
   return (
 
   <section className={styles.container} id='experience'>
-<h2 className={styles.title}> Experience</h2>
+<h2 className={styles.title}> Skills</h2>
 <div className={styles.content}>
   <div className={styles.skills}>
     {skills.map((skill, id) => {
@@ -24,28 +24,7 @@ export const Experience = () => {
       );
     })}
     </div>
-    <ul className={styles.history}> 
-      {history.map((historyItem, id) => {
-return (
-<li key={id} className={styles.historyItem}>
-  <img 
-  src={getImageUrl(historyItem.imageSrc)} 
-  alt={`${historyItem.organisation} Logo`}
-  />
-  <div className={styles.historyItemDetails}>
-    <h3>{`${historyItem.role},${historyItem.organisation}`}</h3>
-  <p>{`${historyItem.startDate}-${historyItem.endDate}`}</p>
-  <ul>
-    {historyItem.experiences.map((experiences, id) => {
-    return<li key={id}>{experiences}</li>;
-  })}
-  </ul>
-  </div>
-</li> 
-        );
-        })}
-      
-    </ul>
+    
   
 </div>
   </section>
